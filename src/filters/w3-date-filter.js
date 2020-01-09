bd9455765141e609f795abf0c3dc76c13d1b2fdc
Andy Bell <me@andy-bell.co.uk>
2020-01-09 16:29:05 +0000

    Fix dates in RSS

--- src/filters/w3-date-filter.js
+++ src/filters/w3-date-filter.js
@@ -1,6 +1,7 @@
 const moment = require('moment');
 
 module.exports = function w3cDate(value) {
+  console.log(value);
   const dateObject = moment.unix(Number(value));
 
   return dateObject.toISOString();
